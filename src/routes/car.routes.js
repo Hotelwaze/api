82784b817a57420a3466f8ecde4bd128148cec53
ralphevano <evanoralph@gmail.com>
2022-06-26 13:47:14 +0800

    cars fix

--- src/routes/car.routes.js
+++ src/routes/car.routes.js
@@ -1,8 +1,17 @@
 import carController from '../controllers/car.controller'
+import {isCustomer, verifyToken} from "../middlewares/auth-jwt";
 
 const carRoutes = (app) => {
-	app.get('/cars/cars', carController.getCars)
-	app.get('/cars/cars-nearby', carController.getCarsNearby)
+  app.get('/cars/cars', carController.getCars)
+  app.get('/cars/cars-nearby', carController.getCarsNearby)
+  app.post('/cars/partners',
+    [
+      verifyToken
+    ], carController.getPartnersCars)
+  app.post('/cars/add',
+    [
+      verifyToken
+    ], carController.addPartnersCars)
 }
 
 export default carRoutes
